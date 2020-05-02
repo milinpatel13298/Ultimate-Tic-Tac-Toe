e1719094299bad52698fe6d68b7242a3494e5bd2
Milin Patel <milinpatel13298@gmail.com>
2020-05-01 20:10:26 -0600

    Added heuristics

--- minimax.py
+++ minimax.py
@@ -61,28 +61,73 @@ def minimax(state, depth, player):
         return row, col, ''
 
     if player == COMP:
-        best = [-1, -1, -inf]
+        best = [-1, -1, float("-inf")]
     else:
-        best = [-1, -1, inf]
+        best = [-1, -1, float("inf")]
 
     if depth == 0 or state.has_tic_tac_toe(COMP) or state.has_tic_tac_toe(HUMAN):
         score = heuristic(state, depth)
         return [-1, -1, score]
-
-    for cell in get_empty_cells(state):
+    """
+    Checks if any of the player is one away from winning in any board and make the appropriate move.
+    """
+    if player==COMP:
+        empty_cells=get_empty_cells(state)
+        dangerous_cells=state.is_one_away_from_tic_tac_toe((player%2)+1)
+        if dangerous_cells:
+            found_dangerous_cells=True
+        else:
+            found_dangerous_cells=False
+            print "no dangerous local boards"
+        favoring_cells=state.is_one_away_from_tic_tac_toe(player)
+        if favoring_cells:
+            found_favoring_cells=True
+        else:
+            found_favoring_cells=False
+            print "no favoring local boards"
+        if found_dangerous_cells==False and found_favoring_cells==False:
+            pass
+        if found_dangerous_cells==False and found_favoring_cells==True:
+            empty_cells[:]=[]
+            for cell in favoring_cells:
+                empty_cells.append(cell)
+        if found_dangerous_cells==True and found_favoring_cells==False:
+            empty_cells[:]=[]
+            for cell in dangerous_cells:
+                empty_cells.append(cell)
+        if found_dangerous_cells==True and found_favoring_cells==True:
+            empty_cells[:]=[]
+            for cell in dangerous_cells:
+                empty_cells.append(cell)
+    else:
+        empty_cells=get_empty_cells(state)
+    for cell in empty_cells:
         row, col = cell[0], cell[1]
         state.board[row][col] = player
         score = minimax(state, depth - 1, (player % 2) + 1)
         state.board[row][col] = 0
         score[0], score[1] = row, col
-
         if player == COMP:
-            if score[2] > best[2]:
-                best = score
+            if score[2] >= best[2]:
+                if score[2]==best[2]:
+                    """
+                    Favors middle positions over sides or corners
+                    MIDDLE > CORNERS > SIDES
+                    """
+                    if (best[0]==0 and best[1]==0) or (best[0]==0 and best[1]==2) or (best[0]==2 and best[1]==0) or (best[0]==2 and best[1]==2):
+                        if score[0]==0 and score[1]==0: #favoring centre position over diagonal position
+                            best=score
+                            print("centre position chosen over diagonal positions")
+                    else:
+                        if ((score[0]==0 and score[1]==1) or (score[0]==1 and score[1]==0) or (score[0]==1 and score[1]==2) or (score[0]==2 and score[1]==1))==0:
+                            best=score  #favoring any position over side position as long as the new position is not a side position too
+                            print("diagonal and centre positions chosen over side positions")
+                else:
+                    best = score
         else:
+            bestMoves=[]
             if score[2] < best[2]:
-                best = score
-
+                best=score
     return best
 
 
@@ -107,9 +152,7 @@ def bot_turn(global_board, bot):
         state = TicTacToeBoard()
         state.board = deepcopy(global_board.board)
         row, col, _ = minimax(state, depth, COMP)
-
         local_board = global_board.local_boards[row * 3 + col]
-
     else:
         # Find the local board in focus
         for lb in global_board.local_boards:
